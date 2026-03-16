839dba9c1e8f8536c7b559447e21a25e306f36c5
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-16 09:46:18 -0500

    entity_empresa

---
+++ src/main/java/com/proyecto/web/entity/Empresa.java
@@ -0,0 +1,25 @@
+package com.proyecto.web.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empresas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Empresa {
+
+    @Id
+    private String nit;
+    private String nombre;
+    private String correo;
+}

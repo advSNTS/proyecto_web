a9d5de4b27aa6a22d0b89a2841313c21eb87a4c9
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-16 12:02:24 -0500

    entidad empleado

---
+++ src/main/java/com/proyecto/web/entity/Empleado.java
@@ -0,0 +1,48 @@
+package com.proyecto.web.entity;
+
+import com.proyecto.web.enums.TipoDocumento;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empleados")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nit", nullable = false)
+    private Empresa empresa;
+
+    private String nombre;
+
+    @Enumerated(EnumType.STRING)
+    private TipoDocumento tipoDocumento;
+
+    private String numeroDocumento;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean deleted = false;
+}

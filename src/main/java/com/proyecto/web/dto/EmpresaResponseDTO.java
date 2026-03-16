05f1095076e3ea624b27c5a2c53b8c7c8299bafe
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-16 09:49:19 -0500

    dto response empresa

---
+++ src/main/java/com/proyecto/web/dto/EmpresaResponseDTO.java
@@ -0,0 +1,18 @@
+package com.proyecto.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmpresaResponseDTO {
+    private String nit;
+    private String nombre;
+    private String correo;
+}

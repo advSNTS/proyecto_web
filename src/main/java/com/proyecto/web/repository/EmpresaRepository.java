d7c57d72a618b26eba982becc8dfc6e0063a5830
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-16 11:31:26 -0500

    repository update

--- src/main/java/com/proyecto/web/repository/EmpresaRepository.java
+++ src/main/java/com/proyecto/web/repository/EmpresaRepository.java
@@ -1,7 +1,12 @@
 package com.proyecto.web.repository;
 
 import com.proyecto.web.entity.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;}
+import java.util.List;
+import java.util.Optional;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, String> {
+    List<Empresa> findAllByDeletedFalse();
+
+    Optional<Empresa> findByNitAndDeletedFalse(String nit);
 }

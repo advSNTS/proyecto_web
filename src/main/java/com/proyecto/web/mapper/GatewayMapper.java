9d06c45c4d519c9e2c5b60dc3b99532f9f84bafd
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-22 13:44:03 -0500

    gateway mapper

---
+++ src/main/java/com/proyecto/web/mapper/GatewayMapper.java
@@ -0,0 +1,27 @@
+package com.proyecto.web.mapper;
+
+import com.proyecto.web.dto.GatewayRequestDTO;
+import com.proyecto.web.dto.GatewayResponseDTO;
+import com.proyecto.web.entity.Gateway;
+import com.proyecto.web.entity.Nodo;
+ 
+public class GatewayMapper {
+ 
+    public static Gateway toEntity(GatewayRequestDTO dto, Nodo nodo) {
+        return Gateway.builder()
+                .nodo(nodo)
+                .tipoGateway(dto.getTipoGateway())
+                .build();
+    }
+ 
+    public static GatewayResponseDTO toResponse(Gateway gateway) {
+        return GatewayResponseDTO.builder()
+                .id(gateway.getId())
+                .nodoId(gateway.getNodo().getId())
+                .nombreNodo(gateway.getNodo().getNombre())
+                .procesoId(gateway.getNodo().getProceso().getId())
+                .nombreProceso(gateway.getNodo().getProceso().getNombre())
+                .tipoGateway(gateway.getTipoGateway())
+                .build();
+    }
+}

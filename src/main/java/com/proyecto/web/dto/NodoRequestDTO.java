694ac2fc48c7f879495d709389b0f237ea29a3ba
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-22 11:21:07 -0500

    nodo request DTO

---
+++ src/main/java/com/proyecto/web/dto/NodoRequestDTO.java
@@ -0,0 +1,21 @@
+package com.proyecto.web.dto;
+
+import com.proyecto.web.enums.TipoNodo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NodoRequestDTO {
+    private Long idProceso;
+    private TipoNodo tipo;
+    private String nombre;
+}
+ 

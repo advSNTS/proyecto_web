ca1827e688728ef1577597f2c8c14e226067d029
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-22 11:56:16 -0500

    actividad req

---
+++ src/main/java/com/proyecto/web/controller/ActividadRequestDTO.java
@@ -0,0 +1,17 @@
+package com.proyecto.web.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActividadRequestDTO {
+    private Long nodoId;
+    private String descripcion;
+}

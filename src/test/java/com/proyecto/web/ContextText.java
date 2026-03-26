f089a9cda1337caaa958e928664a5b2834d7e675
PainterB33 <thomasarevalo123@gmail.com>
2026-03-26 16:49:21 -0500

    Probando configuracion inicial para las pruebas JUnir

---
+++ src/test/java/com/proyecto/web/ContextText.java
@@ -0,0 +1,14 @@
+package com.proyecto.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
